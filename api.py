274d4a1f2ddf5e43b31067a8cc7175948a8917c9
Muhammad Firdaus <vicevirus@Silo.local>
2023-03-29 23:25:47 +0800

    Added redis for better caching

--- api.py
+++ api.py
@@ -1,13 +1,13 @@
 import os
-from cachetools import TTLCache
-from bs4 import BeautifulSoup
-from fastapi.middleware.cors import CORSMiddleware
-import time
 import re
-import requests
 import subprocess
 import json
+import time
+import requests
+from bs4 import BeautifulSoup
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi import FastAPI, Path, HTTPException
+import redis
 
 app = FastAPI()
 
@@ -21,40 +21,29 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+redis_conn = redis.Redis(host='localhost', port=6379, db=0)
 
 @app.get("/")
 async def root():
-	return {"message": "Welcome to UNISEL Timetable API.",
-		"message2": "Dont know to navigate?",
-		"message3": "Take a look at the github link",
-		"link": "https://bit.ly/42LM7Ki"		}
+    return {"message": "Welcome to UNISEL Timetable API.",
+            "message2": "Dont know to navigate?",
+            "message3": "Take a look at the github link",
+            "link": "https://bit.ly/42LM7Ki"}
 
 def validate_semester(semester: str):
     if not re.match(r'^\d{5}$', semester):
         raise HTTPException(
             status_code=500, detail=f"Semester code should have 5 digits")
     return int(semester)
-
-
-cache = TTLCache(maxsize=1000, ttl=100)
-
-
-def validate_semester(semester: str):
-    if not re.match(r'^\d{5}$', semester):
-        raise HTTPException(
-            status_code=500, detail=f"Semester code should have 5 digits")
-    return int(semester)
-
-
 
 
 def get_timetable_data(campus: str, semester: int):
     file_name = f"timetable_data_{semester}_{campus}.json"
-    timetable_data = cache.get(file_name)
+    timetable_data = redis_conn.get(file_name)
 
     if timetable_data is not None:
         # Return cached data
-        return timetable_data
+        return json.loads(timetable_data)
 
     # Check if file exists, update timetable until it is available
     while not os.path.exists(file_name):
@@ -71,8 +60,8 @@ def get_timetable_data(campus: str, semester: int):
     with open(file_name, "r") as file:
         timetable_data = json.load(file)
 
-    # Cache the timetable data for 5 minutes
-    cache[file_name] = timetable_data
+    # Cache the timetable data in Redis for 6 hours
+    redis_conn.setex(file_name, 21600, json.dumps(timetable_data))
 
     return timetable_data
 
@@ -84,13 +73,14 @@ async def read_timetable_data(campus: str, semester: str):
 
     return timetable_data
 
-cache = TTLCache(maxsize=10, ttl=60)
 
 @app.get("/latest_semester_codes")
 async def get_latest_semester_codes():
     # Check if semester codes are already cached
-    if "latest_semester_codes" in cache:
-        return cache["latest_semester_codes"]
+    latest_semester_codes = redis_conn.get("latest_semester_codes")
+
+    if latest_semester_codes is not None:
+        return json.loads(latest_semester_codes)
 
     # If not, scrape the website to get the latest semester codes
     url = "http://etimetable.unisel.edu.my"
@@ -118,7 +108,7 @@ async def get_latest_semester_codes():
 
     for link in f_links:
         f_codes.append(link.text.split()[-1])
-
+        
     sa_codes[0] = sa_codes[0].replace('(', '').replace(')', '')
     bj_codes[0] = bj_codes[0].replace('(', '').replace(')', '')
     f_codes[0] = f_codes[0].replace('(', '').replace(')', '')
@@ -129,11 +119,10 @@ async def get_latest_semester_codes():
         "F": f_codes
     }
 
-    # Cache the semester codes for future requests
-    cache["latest_semester_codes"] = latest_semester_codes
+    # Cache the semester codes in Redis for future requests
+    redis_conn.setex("latest_semester_codes", 60, json.dumps(latest_semester_codes))
 
     return latest_semester_codes
-
 
 if __name__ == "__main__":
 
